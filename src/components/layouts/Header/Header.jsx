2659a5ae0551bbf33058359a3f3eb7e702fabfe6
Nathaly <nathrodriguez222@gmail.com>
2025-03-13 21:29:39 -0500

    Final

--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <>
       {/* HEADER */}
-      <header className="fixed top-0 left-0 w-full bg-gradient-to-r from-purple-600 to-indigo-600 shadow-lg py-4 z-50">
+      <header className="fixed top-0 left-0 w-full h-15 bg-gradient-to-r from-purple-600 to-indigo-600 shadow-lg py-4 z-50">
         <div className="container mx-auto flex items-center justify-between px-4 md:px-8 list-none">
           <h1 className="text-3xl md:text-4xl font-bold text-white tracking-wide">Mi Portafolio</h1>
 
